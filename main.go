28a01ec23104588c260f6ca4ac6fdc4e9096cc93
Sebastian Maldonado <odlam@odlam.local>
2025-03-11 23:17:33 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/binary"
+)
+
+const HEADER = 4
+
+const BTREE_PAGE_SIZE = 4096
+
+const BTREE_MAX_KEY_SIZE = 1000
+
+const BTREE_MAX_VAL_SIZE = 1000
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	assert(node1max <= BTREE_PAGE_SIZE)
+}
+
+type BNode []byte
+
+type Btree struct {
+	root uint64
+	get  func(uint64) []byte
+	new  func([]byte) uint64
+	del  func(uint64)
+}
+
+const (
+	BNODE_NODE = 1
+	BNODE_LEAF = 2
+)
+
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], btype)
+}
+
+func (node BNode) getPtr(idx uint16) uint64 {
+	assert(idx < node.nkeys())
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint64(node[pos:])
+}
+func (node BNode) setPtr(idx uint64, val uint64)
+
+func offsetPos(node BNode, idx uint16) uint16 {
+	assert(1 <= idx && idx <= node.nkeys())
+	return HEADER + 8*node.nkeys() + 2*(idx-1)
+}
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+	return binary.LittleEndian.Uint16(node[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16)
+
+func (node BNode) kvPos(idx uint16) uint16 {
+	assert(idx <= node.nkeys())
+	return HEADER + 8*node.nkeys() + 2*node.nkeys() + node.getOffset(idx)
+}
+
+func (node BNode) getKey(idx uint16) []byte {
+	assert(idx <= node.nkeys())
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node[pos:])
+	return node[pos+4:][:klen]
+}
+
+func (node BNode) getVal(idx uint16) []byte {
+	assert(idx <= node.nkeys())
+	pos := node.kvPos(idx)
+	vlen := binary.LittleEndian.Uint16(node[pos+4+klen:])
+	return node[pos+4+klen+2:][:vlen]
+}
+
+func (node BNode) nbytes() uint16 {
+	return node.kvPos(node.nkeys())
+}
